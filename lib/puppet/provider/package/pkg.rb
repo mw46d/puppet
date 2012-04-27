297f4c68a3affbd23f9e2244792e8466a3775ae6
Marco Walther <mw46d@kenai.com>
2012-04-26 21:27:50 -0700

    New idea to find the pkg version

--- lib/puppet/provider/package/pkg.rb
+++ lib/puppet/provider/package/pkg.rb
@@ -12,12 +12,60 @@ Puppet::Type.type(:package).provide :pkg, :parent => Puppet::Provider::Package d
   # do this here,
   def self.newstylepkgoutput
     unless defined?( @newstylepkgoutput )
-      # This isn't ideal, as it's a SHA1/hash of some ilk, rather than a
-      # real version number so you can't do comparisons on it. I think we
-      # need a more computational way of finding it out.
-      @newstylepkgoutput = ( pkg(:version).chomp == 'a6782843ee0c' )
+      pkg(:search, "-l", "/usr/bin/pkg").each_line do |line|
+         # OpenSolaris build 111b - does not support pkg -H
+         # INDEX      ACTION    VALUE                     PACKAGE
+         # path       file      usr/bin/pkg               pkg:/SUNWipkg@0.5.11-0.111
+         # Solaris 11 11/11 X86
+         # INDEX      ACTION VALUE       PACKAGE
+         # path       file   usr/bin/pkg pkg:/package/pkg@0.5.11-0.175.0.1.0.4.2584
+         if match = /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$/.match(line)
+           unless match[1] == "INDEX" && defined?(@newstylepkgoutput)
+             if match1 = /pkg:([^@]+)@([0-9.]+)-([0-9.]+)$/.match(match[4])
+               # I don't know when the format changed, but my version has the new format
+               @newstylepkgoutput = version_compare({ :version => match1[2], :build => match1[3] },
+                                                    { :version => "0.5.11", :build => "0.175.0.1.0.4.2584" } ) >= 0
+             end
+           end
+         end
+      end
     end
     @newstylepkgoutput
+  end
+
+  def self.version_compare(a, b)
+    # compare the version
+    a_version = a[:version].split('.')
+    b_version = b[:version].split('.')
+
+    l = a_version.length
+    l = b_version.length if b_version.length > a_version.length
+
+    0.upto(l - 1) do |i|
+      if a_version[i] > b_version[i]
+        return 1
+      elsif a_version[i] < b_version[i]
+        return -1
+      end
+    end
+
+    # compare the build
+    a_build = a[:build].split('.')
+    b_build = b[:build].split('.')
+
+    l = a_build.length
+    l = b_build.length if b_build.length > a_build.length
+
+    0.upto(l - 1) do |i|
+      if a_build[i] > b_build[i]
+        return 1
+      elsif a_build[i] < b_build[i]
+        return -1
+      end
+    end
+
+    # OK, both are the same
+    0
   end
 
   def self.instances
@@ -37,10 +85,10 @@ Puppet::Type.type(:package).provide :pkg, :parent => Puppet::Provider::Package d
   end
 
   # Sol 11 version
-  # x11/library/libxcb  1.7-0.175.0.0.0.0.1215     i--
+  # x11/library/libxcb 1.7-0.175.0.0.0.0.1215 i--
 
   self::REGEX = /^(\S+)(?:\s+\(.*?\))?\s+(\S+)\s+(\S+)\s+\S+$/
-  self::SOL11REGEX = /^(\S+)\s+(\S+)\s+(\S+)$/
+  self::SOL11REGEX = /^(\S+)(?:\s+\(.*?\))?\s+(\S+)\s+(\S+)$/
   self::FIELDS = [:name, :version, :status]
 
   def self.parse_line(line)
